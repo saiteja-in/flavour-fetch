f07021496e5939267094409995bf3091585b623e
saiteja-in <vurukondasaiteja13@gmail.com>
2024-07-29 01:13:33 +0530

    connected to mongodb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import cors from 'cors';
-import "dotenv/config"
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
 const app = express();
-app.use(express.json())
+
+app.use(express.json());
 app.use(cors());
-app.get("/test",async(req,res)=>{
-    res.json({message:"Hello world"})
-})
-app.listen(process.env.PORT,()=>{
-    console.log("Server is running on port 7000")  // server is running on port 7000
-})
+
+mongoose.connect(process.env.MONGODB_URL as string)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
+
+app.get("/test", async (req, res) => {
+  res.json({ message: "Hello world" });
+});
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
