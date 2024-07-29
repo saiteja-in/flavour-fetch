c26dcbfcb114aff32b84eea657f0e83ed5fd7b64
saiteja-in <vurukondasaiteja13@gmail.com>
2024-07-29 13:40:14 +0530

    homepage ui

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
+import Layout from './layout/layout'
+import HomePage from './pages/HomePage'
 
 const AppRoutes = () => {
   return (
     <Routes>
-        <Route path="/" element={<span>Home Page</span>} />
+        <Route path="/" element={<Layout><HomePage /></Layout>} />
         <Route path="/user-profile" element={<span>User profile</span>} />
         <Route path="*" element={<Navigate to="/" />} />
 
--- frontend/src/layout/layout.tsx
+++ frontend/src/layout/layout.tsx
@@ -1,8 +1,16 @@
-type Props={
-    children:React.ReactNode;
-}
-const layout =({ children}:Props)=>{
-    return (
-        <div className="flex flex-col min-h-screen"></div>
-    )
-}
+import Header from "@/components/Header";
+import Hero from "@/components/Hero";
+
+type Props = {
+  children: React.ReactNode;
+};
+const Layout = ({ children }: Props) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Hero />
+      <div className="container mx-auto flex-1 py-10">{children}</div>
+    </div>
+  );
+};
+export default Layout;
