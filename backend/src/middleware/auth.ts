357d63cadc56928f953c641aa0058e287a4b1d82
saiteja-in <vurukondasaiteja13@gmail.com>
2024-07-30 23:02:53 +0530

    complete auth and update user functionality (**)

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,7 +1,52 @@
 import { auth } from "express-oauth2-jwt-bearer";
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId: string;
+      auth0Id: string;
+    }
+  }
+}
 
 export const jwtCheck = auth({
-    audience: process.env.AUTH0_AUDIENCE,
-    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
+    audience: 'food-ordering-app-api',
+    issuerBaseURL: 'https://dev-yh00t0c1y2rm0c3t.us.auth0.com/',
     tokenSigningAlg: 'RS256'
   });
+
+export const jwtParse = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+    console.log(req.headers);
+    
+    const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith("Bearer ")) {
+    return res.sendStatus(401);
+  }
+
+  // Bearer lshdflshdjkhvjkshdjkvh34h5k3h54jkh
+  const token = authorization.split(" ")[1];
+
+  try {
+    const decoded = jwt.decode(token) as jwt.JwtPayload;
+    const auth0Id = decoded.sub;
+
+    const user = await User.findOne({ auth0Id });
+
+    if (!user) {
+      return res.sendStatus(401);
+    }
+    req.auth0Id = auth0Id as string;
+    req.userId = user._id.toString();
+    next();
+  } catch (error) {
+    return res.sendStatus(401);
+  }
+};
--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -3,13 +3,14 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './layout/layout'
 import HomePage from './pages/HomePage'
 import AuthCallbackPage from './pages/AuthCallbackPage'
+import UserProfilePage from './pages/UserProfilePage'
 
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/auth-callback" element={<AuthCallbackPage/>}/>
-        <Route path="/" element={<Layout><HomePage /></Layout>} />
-        <Route path="/user-profile" element={<span>User profile</span>} />
+        <Route path="/" element={<Layout showHero><HomePage /></Layout>} />
+        <Route path="/user-profile" element={<Layout ><UserProfilePage/></Layout>} />
         <Route path="*" element={<Navigate to="/" />} />
 
     </Routes>
