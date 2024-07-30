de21c6a63f420116f6104b359bdf39185366bd39
saiteja-in <vurukondasaiteja13@gmail.com>
2024-07-30 17:04:13 +0530

    complete auth0 authentication and tokenization

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -9,7 +9,8 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URI;
-  if (!domain || !clientId || !redirectUri) {
+  const audience=import.meta.env.VITE_AUTH0_AUDIENCE
+  if (!domain || !clientId || !redirectUri ||!audience) {
     throw new Error("unable to initialize auth");
   }
   const onRedirectCallback=(appState?:AppState,user?:User )=>{
@@ -22,7 +23,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      authorizationParams={{ redirect_uri: redirectUri }}
+      authorizationParams={{ redirect_uri: redirectUri,audience }}
       onRedirectCallback={onRedirectCallback}
 
     >{children}</Auth0Provider>
