6901f324ae0cab9137fedef7db83e4a603fb1e15
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-01 15:29:26 +0530

    restaurant model,routes,controller and cloudinary for image storage

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,8 +1,10 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
-import mongoose from 'mongoose';
-import myUserRoute from "./routes/MyUserRoutes"
+import express from "express";
+import { v2 as cloudinary } from 'cloudinary'
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import myUserRoute from "./routes/MyUserRoutes";
+import myRestaurantRoute from "./routes/MyRestaurantRoute"
 dotenv.config();
 
 const app = express();
@@ -10,13 +12,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGODB_URL as string)
+mongoose
+  .connect(process.env.MONGODB_URL as string)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Error connecting to MongoDB:", err));
 
-app.use("/api/my/user",myUserRoute);
+cloudinary.config({
+  cloud_name:"dvl1fcwo2",
+  api_key:"977914559279777",
+  api_secret:"lI0WEcvW-IkB3teWRBEQR5B72t8"
+})
 
-const PORT = process.env.PORT
+app.get("/health", (req, res) => {
+  res.json({ message: "healthy bitch" });
+});
+app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
+
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
