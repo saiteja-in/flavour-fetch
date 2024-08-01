bc35521fc96ce7c10d9362b4ee4f6c85434b5d9e
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-02 00:19:45 +0530

    image section ui

--- frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -6,6 +6,9 @@ import DetailsSection from "./DetailsSection";
 import { Separator } from "@/components/ui/separator";
 import CuisinesSection from "./CuisinesSection";
 import MenuSection from "./MenuSection";
+import ImageSection from "./ImageSection";
+import LoadingButton from "@/components/LoadingButton";
+import { Button } from "@/components/ui/button";
 
 const formSchema = z
   .object({
@@ -72,6 +75,9 @@ const ManageRestaurantForm = ({ onSave, isLoading }: Props) => {
             <CuisinesSection />
             <Separator/>
             <MenuSection/>
+            <Separator/>
+            <ImageSection/>
+            {isLoading ?<LoadingButton/>:<Button type="submit">Submit</Button>}
         </form>
 
     </Form>
