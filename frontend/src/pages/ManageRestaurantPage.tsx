a54b3496f4396558ee90f2598e9718f4e509f8f9
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-02 14:20:31 +0530

    update fetch request

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,14 +1,18 @@
-import { useCreateMyRestaurant, useGetMyRestaurant } from '@/api/MyRestaurantApi'
+import { useCreateMyRestaurant, useGetMyRestaurant, useUpdateMyRestaurant } from '@/api/MyRestaurantApi'
 
 import ManageRestaurantForm from "@/forms/manage-restaurant-form/ManageRestaurantForm";
 import React from 'react'
 
 const ManageRestaurantPage = () => {
-  const {createRestaurant,isLoading}=useCreateMyRestaurant()
+  const {createRestaurant,isLoading:isCreateLoading}=useCreateMyRestaurant()
   const {restaurant}=useGetMyRestaurant()
+  const {updateRestaurant,isLoading:isUpdateLoading}=useUpdateMyRestaurant()
+  const isEditing=!!restaurant;
+  // This line checks if the `restaurant` variable is truthy (i.e., not null or undefined) and assigns the result to `isEditing`. 
+  // This is a shorthand way to determine if the restaurant data is available for editing.
   return (
     <div>
-      <ManageRestaurantForm restaurant={restaurant} onSave={createRestaurant} isLoading={isLoading} />
+      <ManageRestaurantForm  restaurant={restaurant} onSave={isEditing ?updateRestaurant:createRestaurant} isLoading={isCreateLoading || isUpdateLoading} />
     </div>
   )
 }
