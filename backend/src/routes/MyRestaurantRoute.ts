763ca3af665b1a08569bc8aa0eb798cabf64591e
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-02 14:06:56 +0530

    update restaurant api

--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -2,6 +2,7 @@ import express from "express";
 import multer from "multer";
 import MyRestaurantController from "../controllers/MyRestaurantController";
 import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyRestaurantRequest } from "../middleware/validation";
 const router = express.Router();
 
 const storage = multer.memoryStorage();
@@ -16,9 +17,17 @@ router.get("/", jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant);
 router.post(
   "/",
   upload.single("imageFile"),
+  validateMyRestaurantRequest,
   jwtCheck,
   jwtParse,
   MyRestaurantController.createMyRestaurant
 );
+
+router.put("/",upload.single("imageFile"),
+validateMyRestaurantRequest,
+jwtCheck,
+jwtParse,
+MyRestaurantController.updateMyRestaurant
+)
 
 export default router;
