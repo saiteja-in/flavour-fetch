ce315445cf973c3602f67fb545cadda5322e1129
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-04 16:24:19 +0530

    fetch restaurant from api

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,10 +1,12 @@
+import { useSearchRestaurants } from '@/api/RestaurantApi'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const SearchPage = () => {
     const {city}=useParams()
+    const {results}=useSearchRestaurants(city)
   return (
-    <span>user searched for {city}</span>
+    <span>user searched for {results?.data.map(restaurant => restaurant.restaurantName).join(', ')}</span>
   )
 }
 
