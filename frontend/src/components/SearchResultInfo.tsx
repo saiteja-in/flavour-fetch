9eb86491ec0e665be6ad1a130157c9eeab9ea8e0
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-04 18:35:10 +0530

    search page ,cards ui

---
+++ frontend/src/components/SearchResultInfo.tsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+
+type Props={
+    total:number;
+    city:string;
+}
+const SearchResultInfo =({total,city}:Props)=>{
+    return(
+        <div className="text-xl font-bold flex flex-col gap-3 justify-between lg:items-center lg:flex-row">
+            <span>
+                {total === 1 && `${total} Restaurant found in ${city}`}
+                {total !== 1 && `${total} Restaurants found in ${city}`}
+                
+                <Link to="/" className="ml-1 text-sm font-semibold underline cursor-pointer text-blue-500">
+                Change Location
+                </Link>
+            </span>
+        </div>
+        )
+}
+export default SearchResultInfo;
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,13 +1,34 @@
 import { useSearchRestaurants } from '@/api/RestaurantApi'
+import SearchResultCard from '@/components/SearchResultCard'
+import SearchResultInfo from '@/components/SearchResultInfo'
 import React from 'react'
 import { useParams } from 'react-router-dom'
 
 const SearchPage = () => {
     const {city}=useParams()
-    const {results}=useSearchRestaurants(city)
+    const {results,isLoading}=useSearchRestaurants(city)
+    if(isLoading){
+        <span>Loading...</span>
+    }
+    if(!results?.data || ! city){
+        return <span>NO RESULTS FOUND</span>
+    }
   return (
-    <span>user searched for {results?.data.map(restaurant => restaurant.restaurantName).join(', ')}</span>
-  )
+    <div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5' >
+        <div id="cuisines-list">
+            insert cuisines here :)
+
+        </div>
+        <div id="main-content" className='flex flex-col gap-5'>
+        <SearchResultInfo total={results.pagination.total} city={city}/>
+        {results.data.map((restaurant)=>(
+            <SearchResultCard restaurant={restaurant} />
+
+        ))}
+        </div>
+
+    </div>
+)
 }
 
 export default SearchPage
