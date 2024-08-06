66e4a404008f05103b6c2935c8773aecd67bddf0
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-06 20:14:05 +0530

    update

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,20 +1,19 @@
-
+import { Link } from "react-router-dom"
 
 const Footer = () => {
   return (
     <div className='bg-orange-500 py-10'>
         <div className='container mx-auto flex flex-col md:flex-row justify-between items-center'>
-            <span className='text-3xl text-white fond-bold tracking-tight'>
+            <Link to="/" className='text-3xl text-white fond-bold tracking-tight'>
                 FlavourFetch.com
-            </span>
+            </Link>
             <span className='text-white font-bold tracking-tight flex gap-4'>
-                <span>Privacy Policy</span>
-                <span>Terms of Service</span>
+                <Link to="/">Privacy Policy</Link>
+                <Link to="/">Terms of Service</Link>
             </span>
         </div>
       
     </div>
   )
 }
-
 export default Footer
