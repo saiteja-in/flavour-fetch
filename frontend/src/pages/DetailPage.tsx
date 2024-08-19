4dd0329c84c567c6b1723f38506b7a49694a00b7
saiteja-in <vurukondasaiteja13@gmail.com>
2024-08-19 11:28:51 +0530

    displaying menuitems

--- frontend/src/pages/DetailPage.tsx
+++ frontend/src/pages/DetailPage.tsx
@@ -1,4 +1,5 @@
 import { useGetRestaurant } from "@/api/RestaurantApi";
+import MenuItems from "@/components/MenuItem";
 import RestaurantInfo from "@/components/RestaurantInfo";
 import { Card } from "@/components/ui/card";
 import { AspectRatio } from "@radix-ui/react-aspect-ratio";
@@ -22,8 +23,11 @@ const DetailPage = () => {
       <div className="grid md:grid-cols-[4fr_2fr] gap-5 md:px-32">
         <div className="flex flex-col gap-4">
           <RestaurantInfo restaurant={restaurant} />
+        <span className="text-2xl font-bold tracking-tight" >Menu</span>
+        {restaurant.menuItems.map((menuItem)=>(
+            <MenuItems menuItem={menuItem}/>
+        ))}
         </div>
-
       </div>
     </div>
   );
